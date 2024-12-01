cca8a9b8f101780bf3605b264a34e1c0c8157b45
Jakub Grabarczyk <kubagrabarczyk98@gmail.com>
2024-12-01 21:06:58 +0100

    added try except block in get_balance_sheet method

--- xml_parser.py
+++ xml_parser.py
@@ -1,28 +1,35 @@
+"""Module responsible for retrieving financial data from XML files"""
 import xml.etree.ElementTree as ET
 import const as ct
 
 
 class FinancialStatementParser:
+    """Class parses XML files"""
     def __init__(self, xml_file):
+        """Initializes FinancialStatement Parser class."""
         self.xml_file = xml_file
         self.root = self.get_xml_root()
-        self.balance_sheet = self.root.find(ct.BALANCE_TAG, ct.xml_namespaces)
         self.income_statement = self.root.find(ct.INCOME_TAG, ct.xml_namespaces)
 
-    def get_xml_root(self):
+    def get_xml_root(self) -> ET.Element:
         tree = ET.parse(self.xml_file)
         root = tree.getroot()
         return root
 
-    def get_balance_sheet(self):
-        assets = self.balance_sheet.find(ct.ASSETS_TAG, ct.xml_namespaces)
-        liabilities = self.balance_sheet.find(ct.LIABILITIES_TAG, ct.xml_namespaces)
+    def get_balance_sheet(self) -> list[tuple] | None:
+        """Retrieves balance sheet data from xml file"""
+        try:
+            balance_sheet_root = self.root.find(ct.BALANCE_TAG, ct.xml_namespaces)
+            assets = balance_sheet_root.find(ct.ASSETS_TAG, ct.xml_namespaces)
+            liabilities = balance_sheet_root.find(ct.LIABILITIES_TAG, ct.xml_namespaces)
 
-        assets_data = self.loop_balance_sheet_tree(assets)
-        liabilities_data = self.loop_balance_sheet_tree(liabilities)
-        balance_sheet = assets_data + liabilities_data
+            assets_data = self.loop_balance_sheet_tree(assets)
+            liabilities_data = self.loop_balance_sheet_tree(liabilities)
+            balance_sheet = assets_data + liabilities_data
 
-        return balance_sheet
+            return balance_sheet
+        except AttributeError as e:
+            print("Cannot find balance sheet attribute")
 
     def get_income_statement(self):
         statement = self.income_statement.find(ct.PNL_TAG, ct.xml_namespaces)
